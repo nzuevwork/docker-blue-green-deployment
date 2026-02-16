ca910b1d72d50d23b89827cc0cd35ca8769f5d6f
Nikolay <n.zuev.work@gmail.com>
2026-02-16 14:47:06 +0500

    Fix formatting in deploy_blue.sh script

--- scripts/deploy_blue.sh
+++ scripts/deploy_blue.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
 docker network inspect app_net >/dev/null 2>&1 || docker network create app_net >/dev/null
 docker compose -f docker-compose.blue.yml up -d --build
